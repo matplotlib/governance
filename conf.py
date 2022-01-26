3857768a922190d8903bcb83d9d49c12f1998930
Thomas A Caswell <tcaswell@gmail.com>
2022-01-25 22:11:23 -0500

    DEV: add sphinx / myst configuration

---
+++ conf.py
@@ -0,0 +1,89 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from subprocess import Popen, PIPE
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "N2SN Docs"
+copyright = "2022, Matplotlib Development Team"
+author = "Matplotlib Steering Council"
+pipe = Popen("git describe --tags --always", stdout=PIPE, shell=True)
+git = pipe.stdout.read().decode("utf-8").rstrip()
+release = git.lstrip("R")
+version = "-".join(release.split("-")[0:2])
+print(version)
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "myst_parser",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "README.md",
+    "LICENSE.md",
+    "projectlicense.md",
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "mpl_sphinx_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# Fix for table wrapping
+# https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
+
+html_css_files = []
+html_logo = "_static/logo2.svg"
+
+
+# html_sidebars = {"**": ["globaltoc.html", "localtoc.html", "searchbox.html"]}
+
+is_release_build = tags.has("release")  # noqa
+
+
+html_theme_options = {
+    "native_site": True,
+    "logo_link": "index",
+    "collapse_navigation": not is_release_build,
+    # Toc options
+    "navigation_depth": 2,
+}
+
+include_analytics = is_release_build
+if include_analytics:
+    html_theme_options["google_analytics_id"] = "UA-55954603-1"
--- templates.md
+++ templates.md
@@ -1,3 +1,7 @@
+# Templates
+
+## Offer commit rights
+
 This is a template for writing emails to invite people to join the organization.
 
 This should be sent out concurrently with the invitation.
@@ -6,8 +10,7 @@ CC the streeing council.
 
 
 
------
-
+```
 Subject: Matplotlib commit rights
 
 <name>,
@@ -21,11 +24,26 @@ Please see https://matplotlib.org/devel/coding_guide.html#detailed-guidelines
 and remember to be kind during your reviews.
 
 In addition to the discussions on GitHub we have weekly open developer calls
-that you are invited to join (links below) on Thurdays at 1900 UTC <(their timezone if you know it)>.
+that you are invited to join (links below) on Thurdays at <MEETING TIME> UTC
+<(their timezone if you know it)>.
 
 If you accept, welcome (formally) to the team!
 
-Tom (on behalf of the SC)
+YOUR NAME (on behalf of the SC)
 
 Agenda: https://hackmd.io/@matplotlib/SJcAKC2BO
 zoom: https://zoom.us/j/384435716?pwd=WFpxVWxoYXArTDFzN1lWaHNoOE8xZz09
+```
+
+## Announce new committer
+
+In "Announcements" section of discourse
+
+```md
+Welcome <name> to the Matplotlib organization
+----
+
+
+We are happy to announce that <name/@on discourse> (<link to GH profile>) has accepted an invitation to
+join the Matplotlib organization as a committer to core repository!
+```
